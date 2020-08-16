be05fcec68fc6ba74b75ffc75ad31d9ef98ccaff
Thivya1498 <thivyaletchuman1498@gmail.com>
2020-08-16 20:51:26 +0800

    updating the first briefing

---
+++ Mini Project(CUTIES).py
@@ -0,0 +1,36 @@
+from tkinter import*
+
+#using tkinter 
+root = Tk()
+#The title of the window application
+root.title("ATTENDANCE RECORDING DURING COVID-19 PANDEMIC")
+#The size of the window
+root.geometry("700x900")
+
+#welcoming title and its grid
+lbl = Label(root, text = "Welcome to TIES Attendance Recorder",fg="darkblue", font=("Times New Roman",20,"bold","underline"))
+lbl.grid(column=0, row=0, padx=70, pady=40)
+
+#explanation of the application in BM and its grid
+lbl = Label(root, text = "Bagaimana Attendance Recorder berfungsi?",fg="indigo", font=("Times New Roman",14,"bold"))
+lbl.grid(column=0, row=1,pady=5)
+lbl = Label(root, text= "Dengan menggunakan aplikasi ini, anda akan membantu\n KKM untuk mengesan kontak pelajar UniMAP yang berada\n dekat dengan pesakit COVID-19 ataupun suspek COVID-19.", fg="black",font=("Courier",12))
+lbl.grid(column=0, row=2)
+
+#explanation of the application in English and its grid
+lbl = Label(root, text = "How Attendance Recorder works?",fg="indigo", font=("Times New Roman",14,"bold"))
+lbl.grid(column=0, row=3,pady=5)
+lbl =Label(root, text="By using this application, you will help the MOH to\n keep track of the UniMAP students that are in close\n contact with a COVID-19 patient or any suspects\n that might be influenced with COVID-19.",fg="black",font=("Courier",12))
+lbl.grid(column=0,row=4)
+
+#button to next page and its grid
+btn = Button(root, text="Let's Get Started",bg="black",fg="white",font=("starline",14))
+btn.grid(column=0,row=5)
+
+covid_photo = PhotoImage(file="C:\\Users\\user\\Documents\\python codes\\mini project\\image.png")
+lbl=Label(root, image=covid_photo)
+lbl.grid(column=0, row=6)
+
+
+#This will loop the application until the user ends it
+root.mainloop()
