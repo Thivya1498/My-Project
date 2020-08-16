b389a0f5f91a279162347fc057d41e3920022c69
Thivya1498 <thivyaletchuman1498@gmail.com>
2020-08-16 22:58:35 +0800

    Update Mini Project(CUTIES).py

--- Mini Project(CUTIES).py
+++ Mini Project(CUTIES).py
@@ -1,5 +1,5 @@
 from tkinter import*
-
+import tkinter as tk
 #using tkinter 
 root = Tk()
 #The title of the window application
@@ -14,7 +14,7 @@ lbl.grid(column=0, row=0, padx=70, pady=40)
 #explanation of the application in BM and its grid
 lbl = Label(root, text = "Bagaimana Attendance Recorder berfungsi?",fg="indigo", font=("Times New Roman",14,"bold"))
 lbl.grid(column=0, row=1,pady=5)
-lbl = Label(root, text= "Dengan menggunakan aplikasi ini, anda akan membantu\n KKM untuk mengesan kontak pelajar UniMAP yang berada\n dekat dengan pesakit COVID-19 ataupun suspek COVID-19.", fg="black",font=("Courier",12))
+lbl = Label(root, text= "Dengan menggunakan aplikasi ini, anda akan membantu\n KKM untuk mengesan pelajar UniMAP yang mempunyai\n simptom-simptom COVID-19 ataupun kontak rapat\n dengan pesakit COVID-19.", fg="black",font=("Courier",12))
 lbl.grid(column=0, row=2)
 
 #explanation of the application in English and its grid
@@ -22,9 +22,14 @@ lbl = Label(root, text = "How Attendance Recorder works?",fg="indigo", font=("Ti
 lbl.grid(column=0, row=3,pady=5)
 lbl =Label(root, text="By using this application, you will help the MOH to\n keep track of the UniMAP students that are in close\n contact with a COVID-19 patient or any suspects\n that might be influenced with COVID-19.",fg="black",font=("Courier",12))
 lbl.grid(column=0,row=4)
+#creating a new window using function
+def createsecWindow():
+    secWindow= tk.Toplevel(root)
+    lbl=Label(secWindow,text="Name")
+    lbl.grid(column=0,row=0)
 
 #button to next page and its grid
-btn = Button(root, text="Let's Get Started",bg="black",fg="white",font=("starline",14))
+btn = Button(root, text="Let's Get Started",bg="black",fg="white",font=("starline",14),command=createsecWindow)
 btn.grid(column=0,row=5)
 
 covid_photo = PhotoImage(file="C:\\Users\\user\\Documents\\python codes\\mini project\\image.png")
