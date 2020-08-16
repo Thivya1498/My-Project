8b122278a4e10d3f30e653de04e1ff2bbd3e9eb5
Thivya1498 <thivyaletchuman1498@gmail.com>
2020-08-16 15:33:09 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,11 @@
-print(" hai from pc 1 ")
-print(" hi from shaheera ")
-print(" hi from ruby cantik ")
+from tkinter import*
+
+#using tkinter 
+root = Tk()
+#The title of the window application
+root.title("ATTENDANCE RECORDING DURING COVID-19 PANDEMIC")
+#The size of the window
+root.geometry("500x500")
+
+#This will loop the application until the user ends it
+root.mainloop()
