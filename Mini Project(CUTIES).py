e760012bd3d0b7ee0587fe2adccc8928f6f7f4cf
Thivya1498 <thivyaletchuman1498@gmail.com>
2020-08-24 14:24:48 +0800

    updated comment

--- Mini Project(CUTIES).py
+++ Mini Project(CUTIES).py
@@ -15,6 +15,7 @@ root = Tk()
 root.title("ATTENDANCE RECORDING DURING COVID-19 PANDEMIC")
 #The size of the window
 root.geometry("700x900")
+#the background colour of the window
 root['bg']="IndianRed3"
 
 #welcoming title and its grid
@@ -32,19 +33,26 @@ lbl = Label(root, text = "How Attendance Recorder works?",bg="salmon",fg="indigo
 lbl.grid(column=0, row=3,pady=5)
 lbl =Label(root, text="By using this application, you will help the MOH to\n keep track of the UniMAP students that are in close\n contact with a COVID-19 patient or any suspects\n that might be influenced with COVID-19.",bg="yellowgreen",fg="black",font=("Courier",12))
 lbl.grid(column=0,row=4)
+
 #creating a new window using function
 def createsecWindow():
     secWindow = tk.Tk()
+    #title of the new window
+    secWindow.title("Personal Information")
+    #creating tab widget
     tabparent= ttk.Notebook(secWindow)
-    tab1= ttk.Frame(tabparent)
-    tab2= ttk.Frame(tabparent)
-    tab3= ttk.Frame(tabparent)
+    tab1= ttk.Frame(tabparent)#first tab
+    tab2= ttk.Frame(tabparent)#second tab
+    tab3= ttk.Frame(tabparent)#third tab
+
+    #title of the tabs and grid
     tabparent.add(tab1, text = 'Detail Attendance')
     tabparent.add(tab2, text ='Screening Process')
     tabparent.add(tab3, text="Database")
     tabparent.grid()
 
-    def submitForm():
+    #creating a function to save the information in database
+    def saveForm():
         print(entName.get())
     
     
@@ -54,8 +62,8 @@ def createsecWindow():
         #Create a cursor
         c = conn.cursor()
     
-        c.execute("INSERT INTO Attendance(Name,Program,Matric,Phone,BodyTemperature) VALUES(?,?,?,?,?)",
-              (entName.get(),entProgram.get(),entMatric.get(),entPhone.get(),entTemp.get(),))
+        c.execute("INSERT INTO Attendance(Name,Program,Matric,Phone,BodyTemperature,Symptoms) VALUES(?,?,?,?,?,?)",
+              (entName.get(),entProgram.get(),entMatric.get(),entPhone.get(),entTemp.get(),entList.get(),))
 
         #Commit changes
         conn.commit()
@@ -68,8 +76,10 @@ def createsecWindow():
         
         #clear the form after button is hit & show existing data
         clearEntry()
+        clearTab2()
         showExisting()
         
+    #creating a function to show the existing data from database
     def showExisting():
         conn = sqlite3.connect('attendance-book-data.db')  
         c = conn.cursor()
@@ -87,127 +97,144 @@ def createsecWindow():
         for x in records:
             print(x)
             
-            lblExId2 = Label(lfExistingAttendance, text=str(x[0]), bg="white")
+            lblExId2 = Label(lfExistingAttendance, text=str(x[0]),bg="indian red")
             lblExId2.grid(row=rownum,column=0,padx=2,sticky=W)
-            lblExName2 = Label(lfExistingAttendance, text=str(x[1]), bg="white")
+            lblExName2 = Label(lfExistingAttendance, text=str(x[1]),bg="indian red")
             lblExName2.grid(row=rownum,column=1,padx=2,sticky=W)
-            lblExProgram2 = Label(lfExistingAttendance, text=str(x[2]), bg="white")
+            lblExProgram2 = Label(lfExistingAttendance, text=str(x[2]),bg="indian red")
             lblExProgram2.grid(row=rownum,column=2,padx=2,sticky=W)
-            lblExMatric2 = Label(lfExistingAttendance, text=str(x[3]), bg="white")
+            lblExMatric2 = Label(lfExistingAttendance, text=str(x[3]), bg="indian red")
             lblExMatric2.grid(row=rownum,column=3,padx=2,sticky=W)
-            lblExPhone2 = Label(lfExistingAttendance, text=str(x[4]), bg="white")
+            lblExPhone2 = Label(lfExistingAttendance, text=str(x[4]), bg="indian red")
             lblExPhone2.grid(row=rownum,column=4,padx=2,sticky=W)
-            lblExTemp2 = Label(lfExistingAttendance, text=str(x[5]), bg="white")
+            lblExTemp2 = Label(lfExistingAttendance, text=str(x[5]), bg="indian red")
             lblExTemp2.grid(row=rownum,column=5,padx=2,sticky=W)
+            lblExSymptoms2 = Label(lfExistingAttendance, text=str(x[6]), bg="indian red")
+            lblExSymptoms2.grid(row=rownum,column=6,padx=2,sticky=W)
             
             rownum += 1
      
         conn.close()
 
+    #create a function to clear all the entries in tab1
     def clearEntry():
         entName.delete(0,END)
         entProgram.delete(0,END)
         entMatric.delete(0,END)
         entPhone.delete(0,END)
+    
+    #create a function to clear all the entries in tab2
     def clearTab2():
         entTemp.delete(0,END)
         
-
-    #Warning if have risk or not.
+#showing warning sign if have high risks.
     def risk():
-        x=Tk()
-        if entTemp <=37:
-            messagebox.showinfo("Information", "Your Temperature is Normal.") 
-        elif enttemp <=38:
-            messagebox.showinfo("Information", "Attention is need to be given to your Temperature and You can't get in to the Classroom")
+        
+        Temp1=float(entTemp.get())
+        Symptoms=(entList.get())
+        #using if else according to the temperature reading
+        if Temp1<=37.5 and Symptoms=="None":
+            tkinter.messagebox.showinfo("Information", "Low Risk. Your Temperature is Normal. You can enter the Classroom.") 
         else:
-            messagebox.showinfo("Information", "Medicine from docter is needed")
-            
-        x.mainloop()  
+            tkinter.messagebox.showwarning("Information", "High Risk. Your Temperature is Upnormal. You cannot enter the Classroom.")
+             
 #student details (tab 1)
     lblName = Label(tab1, text = "Student's Name  : ")
     lblName.grid(column=0, row=1)
 
-    entName = Entry(tab1, width = 40 )
+    entName = Entry(tab1, width = 40,bg="indian red" )
     entName.grid (column=1, row=1, padx=50, pady=30)
 
     lblProgram = Label(tab1, text = "Program  : ")
     lblProgram.grid(column=0, row=2)
 
-    entProgram = Entry(tab1, width = 40 )
+    entProgram = Entry(tab1, width = 40 ,bg="indian red")
     entProgram.grid (column=1, row=2, padx=50, pady=30)
 
     lblMatric = Label(tab1, text = "Matric No.  : ")
     lblMatric.grid(column=0, row=3)
 
-    entMatric = Entry(tab1, width = 40 )
+    entMatric = Entry(tab1, width = 40,bg="indian red" )
     entMatric.grid (column=1, row=3, padx=50, pady=30)
 
     lblPhone = Label(tab1, text = "Phone No.  : ")
     lblPhone.grid(column=0, row=4)
 
-    entPhone = Entry(tab1, width = 40 )
+    entPhone = Entry(tab1, width = 40, bg="indian red")
     entPhone.grid (column=1, row=4, padx=50, pady=30)
 
-    btnClear1 = Button(tab1, text="Clear",command=clearEntry)
-    btnClear1.grid(column=1,row=5, pady=30)
+    lblNext =Label(tab1,text="*Please proceed to Screening to Process",fg="red")
+    lblNext.grid(column=1,row=5)
+    btnClear1 = Button(tab1, text="Clear",bg="coral1",font=("starline",10),command=clearEntry)
+    btnClear1.grid(column=1,row=6, pady=30)
 
 #screening process (tab 2 )
     lblTemp = Label(tab2, text = "Body Temprature :")
     lblTemp.grid(column=0, row=1)
 
-    entTemp = (Entry(tab2, width = 40 ) )
+    entTemp = Entry(tab2, width = 40 ,bg="indian red") 
     entTemp.grid (column=1, row=1, padx=50, pady=50)
 
     lblSymptom = Label(tab2, text = "Symptoms of Infection : ")
     lblSymptom.grid (column=0, row=2,)
    
-    Checksymptoms1 = Checkbutton (tab2, text="Fever/Demam", takefocus = 0).place(x = 150, y = 120)  
-    Checksymptoms2 = Checkbutton (tab2, text="Dry Cough/Batuk Kering",takefocus = 0).place(x = 150, y =140)   
-    Checksymptoms3 = Checkbutton (tab2, text="Sore Throat/Sakit Tekak",takefocus = 0).place(x = 150, y = 160)  
-    Checksymptoms4 = Checkbutton (tab2, text="Tiredness/Penat", takefocus = 0).place(x = 150, y = 180) 
-    Checksymptoms5 = Checkbutton (tab2, text="Nasal Congestion/Hidung Tersumbat", takefocus = 0).place(x = 150, y = 200) 
-    Checksymptoms6 = Checkbutton (tab2, text="Runny Nose/Hidung Berair",takefocus = 0).place(x = 150, y = 220) 
-    Checksymptoms7 = Checkbutton (tab2, text="Aches and Pains/sakit-sakit badan",takefocus = 0).place(x=150 ,  y = 240) 
-    btnSave = Button(tab2,text='Save',command=submitForm)
-    btnSave.place(x= 150, y=260)
-    btnSubmit = Button(tab2,text='Submit',command=risk)
-    btnSubmit.place(x=190, y=260)
-    btnClear= Button(tab2,text="Clear",command=clearTab2)
-    btnClear.place(x=240,y=260)
+   #using .place for accurate setting
+    Checksymptoms1 = Checkbutton (tab2, text="Fever/Demam", takefocus = 0,bg="coral1").place(x = 150, y = 120)  
+    Checksymptoms2 = Checkbutton (tab2, text="Dry Cough/Batuk Kering",takefocus = 0,bg="coral1").place(x = 150, y =140)   
+    Checksymptoms3 = Checkbutton (tab2, text="Sore Throat/Sakit Tekak",takefocus = 0,bg="coral2").place(x = 150, y = 160)  
+    Checksymptoms4 = Checkbutton (tab2, text="Tiredness/Penat", takefocus = 0,bg="coral2").place(x = 150, y = 180) 
+    Checksymptoms5 = Checkbutton (tab2, text="Nasal Congestion/Hidung Tersumbat", takefocus = 0,bg="coral2").place(x = 150, y = 200) 
+    Checksymptoms6 = Checkbutton (tab2, text="Runny Nose/Hidung Berair",takefocus = 0,bg="coral3").place(x = 150, y = 220) 
+    Checksymptoms7 = Checkbutton (tab2, text="Aches and Pains/sakit-sakit badan",takefocus = 0,bg="coral3").place(x=150 ,  y = 240) 
+    lbllist = Label(tab2, text="*Please list the symptoms below.\nIf there is none then enter 'None'. Thank You.",fg="red")
+    lbllist.place(x=100,y=280)
+    entList= Entry(tab2, width=60)
+    entList.place(x=60,y=320)
+    btnSave = Button(tab2,text='Save',bg="coral1",font=("starline",10),command=saveForm)
+    btnSave.place(x= 150, y=340)
+    btnSubmit = Button(tab2,text='Submit',bg="coral1",font=("starline",10),command=risk)
+    btnSubmit.place(x=190, y=340)
+    btnClear= Button(tab2,text="Clear",bg="coral1",font=("starline",10),command=clearTab2)
+    btnClear.place(x=240,y=340)
+
 #database(tab3)
 
-    btnShowAll = Button(tab3, text="Show All", command=showExisting)
+    btnShowAll = Button(tab3, text="Show All",bg="coral1",font=("starline",10), command=showExisting)
     btnShowAll.grid(column=1,row=0)
 
-    lfExistingAttendance = LabelFrame(tab3, text="My Existing Attendance", bg="white", width=400, height=500)
+    #creating label frame for existing attendance
+    lfExistingAttendance = LabelFrame(tab3, text="My Existing Attendance", bg='indian red', width=400, height=500)
     lfExistingAttendance.grid(row=1, column=1)
 
-    lblExId = Label(lfExistingAttendance, text="Id")
+    lblExId = Label(lfExistingAttendance, text="Id",bg="indian red")
     lblExId.grid(row=2,column=0,padx=2,sticky=W)
-    lblExName = Label(lfExistingAttendance, text="Name")
+    lblExName = Label(lfExistingAttendance, text="Name",bg="indian red")
     lblExName.grid(row=2,column=1,padx=2,sticky=W)
-    lblExProgram = Label(lfExistingAttendance, text="Program")
+    lblExProgram = Label(lfExistingAttendance, text="Program",bg="indian red")
     lblExProgram.grid(row=2,column=2,padx=2,sticky=W)
-    lblExMatric = Label(lfExistingAttendance, text="Matric")
+    lblExMatric = Label(lfExistingAttendance, text="Matric",bg="indian red")
     lblExMatric.grid(row=2,column=3,padx=2,sticky=W)
-    lblExPhone = Label(lfExistingAttendance, text="Phone")
+    lblExPhone = Label(lfExistingAttendance, text="Phone",bg="indian red")
     lblExPhone.grid(row=2,column=4,padx=2,sticky=W)
-    lblExTemp= Label(lfExistingAttendance, text="Body Temperature")
+    lblExTemp= Label(lfExistingAttendance, text="Body Temperature",bg="indian red")
     lblExTemp.grid(row=2,column=5,padx=2,sticky=W)
+    lblExSymptoms= Label(lfExistingAttendance, text="Symptoms",bg="indian red")
+    lblExSymptoms.grid(row=2,column=6,padx=2,sticky=W)
 
+    lblThank= Label(tab3, text="Thank you for using CUTIES Attendance Recorder.\n Stay Safe, Stay Healthy and Please wear Mask",bg="coral1",fg="darkblue", font=("Times New Roman",15,"underline"))
+    lblThank.grid(column=1, row=12,padx=20)
 
 #button to next page and its grid
 btn = Button(root, text="Let's Get Started",bg="black",fg="white",font=("starline",14),command=createsecWindow)
 btn.grid(column=0,row=5)
 
+#creating image in the first window.
 img= ImageTk.PhotoImage(Image.open("covid.png"))
 imgDisplay=Label(root, image=img,bg="IndianRed3")
 imgDisplay.grid(column=0, row=6)
 img1= ImageTk.PhotoImage(Image.open("image.jpg"))
 imgDisplay=Label(root, image=img1,bg="IndianRed3")
 imgDisplay.grid(column=0, row=7)
-
 
 #This will loop the application until the user ends it
 root.mainloop()
